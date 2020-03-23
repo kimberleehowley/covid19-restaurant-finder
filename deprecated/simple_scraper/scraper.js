72a43c088c45d1f5095b6346900bf31a865823a5
Kimberlee Johnson <kimee.johnson@gmail.com>
2020-03-22 17:35:21 -0700

    Updated folder structure

---
+++ deprecated/simple_scraper/scraper.js
@@ -0,0 +1,16 @@
+// DEPRECATED 
+
+// I attempted to build a simple scraper to pull restaurant data 
+// But that wound up taking more time than copy/pasting from one of the links in the page source code (for now).
+
+// Importing libraries for making http requests and parsing results 
+// const axios = require('axios'); 
+// const cheerio = require('cheerio'); 
+
+// Making http request
+// axios.get('https://projects.sfchronicle.com/2020/restaurant-delivery/')
+    // .then(res => {
+        // const $ = cheerio.load(res.data); 
+
+        // console.log($('#row-1').first().text()); 
+    // })
