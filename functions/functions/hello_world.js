f1d03eb6195d278d6fc835f4a50af5fd814cb7fc
Kimberlee Johnson <kimee.johnson@gmail.com>
2020-03-23 19:53:55 -0700

    Added library to test Twilio functions on local env

--- functions/functions/hello_world.js
+++ functions/functions/hello_world.js
@@ -1,13 +1,6 @@
-const got = require('got');
-
 exports.handler = function(context, event, callback) {
     let twiml = new Twilio.twiml.MessagingResponse();
-   
-    got('http://localhost:5000/restaurants', {json: true}).then(response => {
-      twiml.message(response.body);
-      callback(null, twiml);
-    }).catch(error => {
-      console.log(error.response.body);
-      callback(error);
-    });
-   };
+    twiml.message('Thanks for eating local! Looking up restaurants in your neighborhood...');
+    twiml.message('Test!'); 
+    callback(null, twiml);
+  };
