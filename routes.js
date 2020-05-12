66ebaf9a0e55d99cd6b53aecbb3093bcc418b4ec
Kimberlee Johnson <kimee.johnson@gmail.com>
2020-05-11 20:08:09 -0700

    fix: trying again with join

--- routes.js
+++ routes.js
@@ -6,9 +6,6 @@ const bodyParser = require("body-parser");
 
 router.use(bodyParser.urlencoded({ extended: false }));
 
-// Load zip codes as a constant 
-const validZips = await model.getZips(); 
-
 // Helper function: wraps another function in try/catch and passes errors to middleware
 function asyncHandler(cb) {
   return async (req, res, next) => {
@@ -20,7 +17,7 @@ function asyncHandler(cb) {
   };
 }
 
-// Display all restaurant names 
+// Display all restaurant names
 router.get(
   "/restaurants",
   asyncHandler(async (req, res) => {
@@ -29,7 +26,7 @@ router.get(
   })
 );
 
-// Display all restaurants in a zip code 
+// Display all restaurants in a zip code
 router.get(
   "/restaurants/:zip",
   asyncHandler(async (req, res) => {
@@ -44,31 +41,39 @@ router.get(
   })
 );
 
-// Receive a zip code, and create a new message based on that zip 
+// Receive a zip code, and create a new message based on that zip
 router.post(
   "/sms",
   asyncHandler(async (req, res) => {
     const twiml = new MessagingResponse();
 
-    // Store whatever is sent to the POST request as a variable 
-    // Variable named 'zip' here, because user ideally texting a zip 
+    // Store whatever is sent to the POST request as a variable
+    // Variable named 'zip' here, because user ideally texting a zip
     const zip = req.body.Body;
+
+    // Load zip codes as a constant
+    const validZips = await model.getZips();
 
     // But, if the zip isn't in our list
     if (!validZips.includes(zip)) {
-      // Send an error message 
-      twiml.message(`Hmmm, I'm not finding any restaurants open in ${req.body.Body}`);
-      twiml.message(`Could you please try another five-digit Bay Area zip code?`);
+      // Send an error message
+      twiml.message(
+        `Hmmm, I'm not finding any restaurants open in ${req.body.Body}`
+      );
+      twiml.message(
+        `Could you please try another five-digit Bay Area zip code?`
+      );
       // But if it is in our list
     } else {
-      // Load the restaurants for that zip code 
+      // Load the restaurants for that zip code
       const restaurants_in_zip = await model.getRestaurant(zip);
-      const formatted_list = restaurants_in_zip.map(restaurant => `${restaurant}\n\n`).join(""); 
+      const formatted_list = restaurants_in_zip
+        .map(restaurant => `${restaurant}\n\n`)
+        .join("");
       twiml.message(
         `Thanks for eating local❣️ Here are the restaurants open in ${req.body.Body}:`
       );
       twiml.message(formatted_list.toString());
-
     }
 
     res.writeHead(200, { "Content-Type": "text/xml" });
